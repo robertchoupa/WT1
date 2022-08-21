e4914a2a72153c5c2aa469417166cf65736aaedf
robertchoupa <robertchoupa@gmail.com>
2022-08-21 11:45:39 +0200

    Les Commentaires

--- src/main/java/cm/python/Test.java
+++ src/main/java/cm/python/Test.java
@@ -1,5 +1,9 @@
+/*
+ * Les Commentaires
+ */
 package cm.pythou;
 public class Test{
+    //Une nouvelle ligne de commentaire
     public static void main(String[]args){
         System.out.println("Hello!");
     }
